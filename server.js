958f29381f213c6abc8ae27cef98efb9a9fdffd3
Oleg Hansen <oleg-ucb@outlook.com>
2019-01-22 23:20:27 -0800

    updated the port

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var axios = require("axios");
 var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
-var PORT = 3003;
+var PORT = 8080;
 // Initialize Express
 var app = express();
 // Configure middleware
