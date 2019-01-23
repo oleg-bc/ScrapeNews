d2766be6ed17b37a595b55c34d103fa5a7333180
Oleg Hansen <oleg-ucb@outlook.com>
2019-01-22 23:49:54 -0800

    working to resolve heroku issues commented out console.log

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ var request = require('request');
 request('http://www.wired.com', function (error, response, body) {
   console.log('error:', error); // Print the error if one occurred
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the wired homepage.
+  //console.log('body:', body); // Print the HTML for the wired homepage.
 });
 
 // A GET route for scraping the echoJS website
